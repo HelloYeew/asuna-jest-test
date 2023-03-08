46fad3a3adc87ff13487163f8bb4ee645d7c6a9d
Phawit Pornwattanakul <me@helloyeew.dev>
2023-03-09 01:10:48 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function sumNumber(a, b) {
+    return a + b;
+}
+
+function minusNumber(a, b) {
+    return a - b;
+}
+
+module.exports = {
+    sumNumber,
+    minusNumber
+}
---
+++ index.test.js
@@ -0,0 +1,7 @@
+const index = require("./index");
+
+
+// Create easy test for sumNumber function
+test("sumNumber function", () => {
+    expect(index.sumNumber(1, 2)).toBe(3)
+})
